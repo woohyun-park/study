35cf2a070ece2d635f0cfec5ae6f203d4a31f536
Woohyun Park <woohyunpark.mail@gmail.com>
2022-10-05 12:51:32 +0900

    STUDY, study extra space below image

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import "./styles.css";
-import { useState } from "react";
-import { initialTabs as tabs } from "./ingredients";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function App() {
-  const [selectedTab, setSelectedTab] = useState(tabs[0]);
-
   return (
-    <div className="window">
-      <nav>
-        <ul>
-          {tabs.map((item) => (
-            <li
-              key={item.label}
-              className={item === selectedTab ? "selected" : ""}
-              onClick={() => setSelectedTab(item)}
-            >
-              {`${item.icon} ${item.label}`}
-              {item === selectedTab ? (
-                <motion.div className="underline" layoutId="underline" />
-              ) : null}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <main>
-        <AnimatePresence exitBeforeEnter>
-          <motion.div
-            key={selectedTab ? selectedTab.label : "empty"}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -10, opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            {selectedTab ? selectedTab.icon : "😋"}
-          </motion.div>
-        </AnimatePresence>
-      </main>
-    </div>
+    <>
+      <div class="container">
+        <img
+          src="https://images.unsplash.com/photo-1584380497382-2722e59ff33f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=687&q=80"
+          alt=""
+        />
+      </div>
+    </>
   );
 }
